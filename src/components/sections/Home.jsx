714f66d082c2052986d4745dbcb174e56cb16cd5
MamounMohamed <mamoun21cool@gmail.com>
2025-05-15 01:55:53 +0300

    Update Home.jsx

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Navbar from "../Navbar";
 
 export default function Home() {
@@ -16,12 +17,12 @@ export default function Home() {
                 </p>
 
                 <div className="flex items-center justify-center space-x-4 mt-8">
-                    <a href="/#projects" className="bg-blue-500 text-white py-3 px-6 rounded hover:bg-blue-800  overflow-hidden hover:translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,138,246,0.4)] transition transistion-all duration-300">
+                    <Link to="/projects" className="bg-blue-500 text-white py-3 px-6 rounded hover:bg-blue-800  overflow-hidden hover:translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,138,246,0.4)] transition transistion-all duration-300">
                         View Projects
-                    </a>
-                    <a href="/#contact" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded  overflow-hidden hover:translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,138,246,0.4)] transition transistion-all duration-300"> 
+                    </Link>
+                    <Link to="/contact" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded  overflow-hidden hover:translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,138,246,0.4)] transition transistion-all duration-300"> 
                         Contact Me
-                    </a>
+                    </Link>
                     </div>
             </div>
         </section>
